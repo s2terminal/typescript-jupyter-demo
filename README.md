9ee1c0473d50a5496c266045a22c63ed66d980ce
s2terminal <suzuki.sh@s2terminal.com>
2025-04-21 16:02:26 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Jupyter+Deno
+### [mise](https://mise.jdx.dev/)の場合
+
+```
+$ uv run jupyter lab
+```
+
+localhost:8888
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from typescript-jupyter-demo!")
+
+
+if __name__ == "__main__":
+    main()
